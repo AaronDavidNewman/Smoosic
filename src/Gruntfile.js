f7a8b0b797d6411fbbd537597c714a2e947286c5
Newman, Aaron <anewman@air.org>
2019-05-20 08:09:48 -0500

    add makefile
    add makefile

---
+++ src/Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['src/common/vexHelpers.js','src/common/svgHelpers.js','src/common/htmlHelpers.js',
+		   '/src/smo/data/note.js','/src/smo/data/measure.js','/src/smo/data/systemStaff.js','/src/smo/data/staffModifiers.js',
+		   '/src/smo/xform/iterator.js','/src/smo/xform/modifiers.js','/src/smo/xform/tickDuration.js','/src/smo/xform/selections.js',
+		   '/src/smo/xform/operations.js','src/vex/vxMeasure.js','src/vex/vxSystem.js',
+		   '/src/ui/tracker.js','/src/ui/layout.js','/src/ui/editor.js','/src/ui/menus.js','/src/ui/utController.js',
+		   '/src/ui/dialog.js','src/ui/controller.js'],
+        dest: 'build/<%= pkg.name %>.js'
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  // Default task(s).
+  grunt.registerTask('default', ['concat']);
+  };
