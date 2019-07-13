2f5741c2628209e06dad5af747bfedb3c9855d92
Aaron Newman <anewman@air.org>
2019-07-13 12:15:28 -0500

    fi typo

--- build/smoosic.js
+++ build/smoosic.js
@@ -988,183 +988,88 @@ class SmoBeamGroup {
     }
 }
 ;
-// # This file contains modifiers that might take up multiple measures, and are thus associated
-// with the staff.
-
-class StaffModifierBase {
+class SmoNoteModifierBase {
 	constructor(ctor) {
 		this.ctor=ctor;
-	}
+	}	
 	static deserialize(jsonObj) {
-		var params = JSON.parse(json)
-		var ctor = eval(dbType);
-		var rv= new ctor(jsonObj);
+		var ctor = eval(jsonObj.ctor);
+		var rv = new ctor(jsonObj);
 		rv.attrs.id=jsonObj.attrs.id;
 		rv.attrs.type=jsonObj.attrs.type;
-
-	}
+	}	
 }
-// ## SmoStaffHairpin
-// ## Descpription:
-// crescendo/decrescendo
-class SmoStaffHairpin extends StaffModifierBase {
-    constructor(params) {
-		super('SmoStaffHairpin');
-        Vex.Merge(this, SmoStaffHairpin.defaults);
-        smoMusic.filteredMerge(['position', 'xOffset', 'yOffset', 'hairpinType', 'height'], params, this);
-        this.startSelector = params.startSelector;
-        this.endSelector = params.endSelector;
 
-        if (!this['attrs']) {
+// ## SmoDynamicText
+// ## Description:
+// standard dynamics text
+class SmoDynamicText extends SmoNoteModifierBase {
+	 static get defaults() {
+        return {
+            xOffset: 0,
+            fontSize: 38,
+            yOffsetLine:11,
+			yOffsetPixels:0,
+            text: SmoDynamicText.dynamics.MP,
+        };
+    }
+	
+	static get dynamics() {
+        // matches VF.modifier
+        return {
+			PP:'pp',
+			P:'p',
+            MP: 'mp',
+            MF: 'mf',
+            F: 'f',
+            FF: 'ff',
+			SFZ:'sfz'
+        };
+    }
+	
+	constructor(parameters) {
+		super('SmoDynamicText');
+		Vex.Merge(this, SmoDynamicText.defaults);
+        smoMusic.filteredMerge(SmoDynamicText.attrArray, parameters, this);
+        this.selector = parameters.selector;
+		
+		 if (!this['attrs']) {
             this.attrs = {
                 id: VF.Element.newID(),
-                type: 'SmoStaffHairpin'
+                type: 'SmoDynamicText'
             };
         } else {
             console.log('inherit attrs');
         }
-    }
-	get serialize() {
-		var params={};
-		smoMusic.filteredMerge(['position', 'xOffset', 'yOffset', 'hairpinType', 'height'], this, params);
-		return params;
 	}
-    get id() {
-        return this.attrs.id;
-    }
-    get type() {
-        return this.attrs.type;
-    }
-
-    backupOriginal() {
+	get id() {
+		return this.attrs.id;
+	}
+	set id(ignore) {}
+	get type() {
+		return this.attrs.type;
+	}
+	set type(ignore) {}
+	static get attrArray() {
+		return ['xOffset', 'fontSize', 'yOffsetLine', 'yOffsetPixels', 'text'];
+	}
+	backupOriginal() {
         if (!this['original']) {
             this.original = {};
             smoMusic.filteredMerge(
-                ['xOffsetLeft', 'xOffsetRight', 'yOffset', 'height', 'position', 'hairpinType'],
+                SmoDynamicText.attrArray,
                 this, this.original);
         }
     }
     restoreOriginal() {
         if (this['original']) {
             smoMusic.filteredMerge(
-                ['xOffsetLeft', 'xOffsetRight', 'yOffset', 'height', 'position', 'hairpinType'],
+                SmoDynamicText.attrArray,
                 this.original, this);
             this.original = null;
         }
     }
-    static get defaults() {
-        return {
-            xOffsetLeft: -2,
-            xOffsetRight: 0,
-            yOffset: -15,
-            height: 10,
-            position: SmoStaffHairpin.positions.BELOW,
-            hairpinType: SmoStaffHairpin.types.CRESCENDO
-
-        };
-    }
-    static get positions() {
-        // matches VF.modifier
-        return {
-            LEFT: 1,
-            RIGHT: 2,
-            ABOVE: 3,
-            BELOW: 4,
-        };
-    }
-    static get types() {
-        return {
-            CRESCENDO: 1,
-            DECRESCENDO: 2
-        };
-    }
-}
-
-class SmoSlur extends StaffModifierBase{
-    static get defaults() {
-        return {
-            spacing: 2,
-            thickness: 2,
-            xOffset: 0,
-            yOffset: 10,
-            position: SmoSlur.positions.HEAD,
-            position_end: SmoSlur.positions.HEAD,
-            invert: false,
-            cp1x: 0,
-            cp1y: 40,
-            cp2x: 0,
-            cp2y: 40
-        };
-    }
-
-    // matches VF curve
-    static get positions() {
-        return {
-            HEAD: 1,
-            TOP: 2
-        };
-    }
-    static get parameterArray() {
-        return ['spacing', 'xOffset', 'yOffset', 'position', 'position_end', 'invert',
-            'cp1x', 'cp1y', 'cp2x', 'cp2y'];
-    }
-	
-	get serialize() {
-		var params={};
-		smoMusic.filteredMerge(SmoSlur.parameterArray, this, params);
-		return params;
-	}
-
-    backupOriginal() {
-        if (!this['original']) {
-            this.original = {};
-            smoMusic.filteredMerge(
-                SmoSlur.parameterArray,
-                this, this.original);
-        }
-    }
-    restoreOriginal() {
-        if (this['original']) {
-            smoMusic.filteredMerge(
-                SmoSlur.parameterArray,
-                this.original, this);
-            this.original = null;
-        }
-    }
-    get controlPoints() {
-        var ar = [{
-                x: this.cp1x,
-                y: this.cp1y
-            }, {
-                x: this.cp2x,
-                y: this.cp2y
-            }
-        ];
-        return ar;
-    }
-
-    get type() {
-        return this.attrs.type;
-    }
-    get id() {
-        return this.attrs.id;
-    }
-
-    constructor(params) {
-		super('SmoSlur');
-        Vex.Merge(this, SmoSlur.defaults);
-        smoMusic.filteredMerge(SmoSlur.parameterArray, params, this);
-        this.startSelector = params.startSelector;
-        this.endSelector = params.endSelector;
-        if (!this['attrs']) {
-            this.attrs = {
-                id: VF.Element.newID(),
-                type: 'SmoSlur'
-            };
-        }
-    }
-}
-;
+};
 // # SmoMeasure - data for a measure of music
 // # Description:
 // Many rules of musical engraving are enforced at a measure level, e.g. the duration of 
@@ -2016,6 +1921,183 @@ class SmoScore {
             }
         }
         return null;
+    }
+}
+;
+// # This file contains modifiers that might take up multiple measures, and are thus associated
+// with the staff.
+
+class StaffModifierBase {
+	constructor(ctor) {
+		this.ctor=ctor;
+	}
+	static deserialize(jsonObj) {
+		var params = JSON.parse(json)
+		var ctor = eval(dbType);
+		var rv= new ctor(jsonObj);
+		rv.attrs.id=jsonObj.attrs.id;
+		rv.attrs.type=jsonObj.attrs.type;
+
+	}
+}
+// ## SmoStaffHairpin
+// ## Descpription:
+// crescendo/decrescendo
+class SmoStaffHairpin extends StaffModifierBase {
+    constructor(params) {
+		super('SmoStaffHairpin');
+        Vex.Merge(this, SmoStaffHairpin.defaults);
+        smoMusic.filteredMerge(['position', 'xOffset', 'yOffset', 'hairpinType', 'height'], params, this);
+        this.startSelector = params.startSelector;
+        this.endSelector = params.endSelector;
+
+        if (!this['attrs']) {
+            this.attrs = {
+                id: VF.Element.newID(),
+                type: 'SmoStaffHairpin'
+            };
+        } else {
+            console.log('inherit attrs');
+        }
+    }
+	get serialize() {
+		var params={};
+		smoMusic.filteredMerge(['position', 'xOffset', 'yOffset', 'hairpinType', 'height'], this, params);
+		return params;
+	}
+    get id() {
+        return this.attrs.id;
+    }
+    get type() {
+        return this.attrs.type;
+    }
+
+    backupOriginal() {
+        if (!this['original']) {
+            this.original = {};
+            smoMusic.filteredMerge(
+                ['xOffsetLeft', 'xOffsetRight', 'yOffset', 'height', 'position', 'hairpinType'],
+                this, this.original);
+        }
+    }
+    restoreOriginal() {
+        if (this['original']) {
+            smoMusic.filteredMerge(
+                ['xOffsetLeft', 'xOffsetRight', 'yOffset', 'height', 'position', 'hairpinType'],
+                this.original, this);
+            this.original = null;
+        }
+    }
+    static get defaults() {
+        return {
+            xOffsetLeft: -2,
+            xOffsetRight: 0,
+            yOffset: -15,
+            height: 10,
+            position: SmoStaffHairpin.positions.BELOW,
+            hairpinType: SmoStaffHairpin.types.CRESCENDO
+
+        };
+    }
+    static get positions() {
+        // matches VF.modifier
+        return {
+            LEFT: 1,
+            RIGHT: 2,
+            ABOVE: 3,
+            BELOW: 4,
+        };
+    }
+    static get types() {
+        return {
+            CRESCENDO: 1,
+            DECRESCENDO: 2
+        };
+    }
+}
+
+class SmoSlur extends StaffModifierBase{
+    static get defaults() {
+        return {
+            spacing: 2,
+            thickness: 2,
+            xOffset: 0,
+            yOffset: 10,
+            position: SmoSlur.positions.HEAD,
+            position_end: SmoSlur.positions.HEAD,
+            invert: false,
+            cp1x: 0,
+            cp1y: 40,
+            cp2x: 0,
+            cp2y: 40
+        };
+    }
+
+    // matches VF curve
+    static get positions() {
+        return {
+            HEAD: 1,
+            TOP: 2
+        };
+    }
+    static get parameterArray() {
+        return ['spacing', 'xOffset', 'yOffset', 'position', 'position_end', 'invert',
+            'cp1x', 'cp1y', 'cp2x', 'cp2y'];
+    }
+	
+	get serialize() {
+		var params={};
+		smoMusic.filteredMerge(SmoSlur.parameterArray, this, params);
+		return params;
+	}
+
+    backupOriginal() {
+        if (!this['original']) {
+            this.original = {};
+            smoMusic.filteredMerge(
+                SmoSlur.parameterArray,
+                this, this.original);
+        }
+    }
+    restoreOriginal() {
+        if (this['original']) {
+            smoMusic.filteredMerge(
+                SmoSlur.parameterArray,
+                this.original, this);
+            this.original = null;
+        }
+    }
+    get controlPoints() {
+        var ar = [{
+                x: this.cp1x,
+                y: this.cp1y
+            }, {
+                x: this.cp2x,
+                y: this.cp2y
+            }
+        ];
+        return ar;
+    }
+
+    get type() {
+        return this.attrs.type;
+    }
+    get id() {
+        return this.attrs.id;
+    }
+
+    constructor(params) {
+		super('SmoSlur');
+        Vex.Merge(this, SmoSlur.defaults);
+        smoMusic.filteredMerge(SmoSlur.parameterArray, params, this);
+        this.startSelector = params.startSelector;
+        this.endSelector = params.endSelector;
+        if (!this['attrs']) {
+            this.attrs = {
+                id: VF.Element.newID(),
+                type: 'SmoSlur'
+            };
+        }
     }
 }
 ;
