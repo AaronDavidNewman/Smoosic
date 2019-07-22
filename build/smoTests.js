e9671c0c6881e53d1ce428ff415358d35bb30f7d
AaronDavidNewman <anewman@air.org>
2019-07-21 22:09:54 -0500

    rebuild

--- build/smoTests.js
+++ build/smoTests.js
@@ -288,141 +288,148 @@ class ChordTest {
 }
 ;
 class UndoTest {
-   
-    static CommonTests() {
+
+	static CommonTests() {
 		$('h1.testTitle').text('Undo Test');
-		var score=SmoScore.getEmptyScore();
-		
+		var score = SmoScore.getEmptyScore();
+
 		var pasteBuffer = new PasteBuffer();
-		
-        score.addDefaultMeasureWithNotes(0,{});
-        score.addDefaultMeasureWithNotes(1,{});
-        score.addDefaultMeasureWithNotes(2,{});
-		
-		var serial = JSON.stringify(score.serialize(),null,'');
+
+		score.addDefaultMeasureWithNotes(0, {});
+		score.addDefaultMeasureWithNotes(1, {});
+		score.addDefaultMeasureWithNotes(2, {});
+
+		var serial = JSON.stringify(score.serialize(), null, '');
 		console.log(serial);
-        var keys = utController.createUi(document.getElementById("boo"),score);
+		var keys = utController.createUi(document.getElementById("boo"), score);
 		var undo = keys.undoBuffer;
 		var score = keys.score;
-		var layout = keys.layout;		
-		
+		var layout = keys.layout;
+
 		var detach = () => {
 			keys.detach();
-			keys=null;
-			score=null;
-			layout=null;
+			keys = null;
+			score = null;
+			layout = null;
 		}
 		var subTitle = (txt) => {
 			$('.subTitle').text(txt);
 		}
 
-        var timeTest = () => {
-            const promise = new Promise((resolve, reject) => {
-                    setTimeout(() => {
-                        resolve();
-                    },
-                        200);
-                });
-            return promise;
-        }
+		var timeTest = () => {
+			const promise = new Promise((resolve, reject) => {
+					setTimeout(() => {
+						resolve();
+					},
+						200);
+				});
+			return promise;
+		}
 
-        var signalComplete = () => {
+		var signalComplete = () => {
 			detach();
-            return timeTest();
-        }
+			return timeTest();
+		}
 
-        var drawDefaults = () => {
-            // music.notes = VX.APPLY_MODIFIERS (music.notes,staffMeasure.keySignature);
-            // measure.applyModifiers();
-            keys.render();
-            return timeTest();
-        }
-					
-        var changePitch = () => {
+		var drawDefaults = () => {
+			// music.notes = VX.APPLY_MODIFIERS (music.notes,staffMeasure.keySignature);
+			// measure.applyModifiers();
+			keys.render();
+			return timeTest();
+		}
+
+		var changePitch = () => {
 			subTitle('changePitch');
-            var target = SmoSelection.pitchSelection(layout.score,0,2, 0, 1,[0]);
-			SmoUndoable.setPitch(target,{
-                        letter: 'e',
-                        octave: 4,
-                        accidental: 'b'
-                    },undo);
-            /* if (target) {
-                target.note.pitches = [{
-                        letter: 'e',
-                        octave: 4,
-                        accidental: 'b'
-                    }
-                ];
-            }   */
-            keys.render();
-            return timeTest();
-        }
+			var target = SmoSelection.pitchSelection(layout.score, 0, 2, 0, 1, [0]);
+			SmoUndoable.setPitch(target, {
+				letter: 'e',
+				octave: 4,
+				accidental: 'b'
+			}, undo);
+			/* if (target) {
+			target.note.pitches = [{
+			letter: 'e',
+			octave: 4,
+			accidental: 'b'
+			}
+			];
+			}   */
+			keys.render();
+			return timeTest();
+		}
 		var undoTest = () => {
 			var buf = undo.peek();
-			subTitle('undo '+buf.title);
+			subTitle('undo ' + buf.title);
 			layout.undo(undo);
-            return timeTest();
+			return timeTest();
 		}
-        var changePitch2 = () => {
+		var changePitch2 = () => {
 			subTitle('change pitch to f#, rest');
-            var target = SmoSelection.pitchSelection(score,0,1, 0, 1, [0]);
-			SmoUndoable.setPitch(target,{
-					letter: 'f',
-					octave: 4,
-					accidental: '#'
-				},undo);
-			            
-			SmoUndoable.makeRest(SmoSelection.noteSelection(score,0,1,0,2),undo);
-            keys.render();
-            return timeTest();
-        }
-		var keySigTest= () => {
+			var target = SmoSelection.pitchSelection(score, 0, 1, 0, 1, [0]);
+			SmoUndoable.setPitch(target, {
+				letter: 'f',
+				octave: 4,
+				accidental: '#'
+			}, undo);
+
+			SmoUndoable.makeRest(SmoSelection.noteSelection(score, 0, 1, 0, 2), undo);
+			keys.render();
+			return timeTest();
+		}
+		var keySigTest = () => {
 			subTitle('key sig to A');
-			var selection = SmoSelection.measureSelection(score,0,1);
-			SmoUndoable.addKeySignature(score,selection,'A',undo);
+			var selection = SmoSelection.measureSelection(score, 0, 1);
+			SmoUndoable.addKeySignature(score, selection, 'A', undo);
 			// score.addKeySignature(1,'A');
-			var selection = SmoSelection.noteSelection(score,0,1,0,2);
-			SmoUndoable.makeNote(selection,undo);
+			var selection = SmoSelection.noteSelection(score, 0, 1, 0, 2);
+			SmoUndoable.makeNote(selection, undo);
 			keys.render();
 			return timeTest();
 		}
 		var crescendoTest = () => {
-            var ft = SmoSelection.noteSelection(layout.score, 0, 0, 0, 0);
-            var tt = SmoSelection.noteSelection(layout.score, 0, 0, 0, 3);
-            SmoUndoable.crescendo(ft, tt,undo);
-            layout.render();
-            return timeTest();
-        }
-		var keySigTest2= () => {
+			var ft = SmoSelection.noteSelection(layout.score, 0, 0, 0, 0);
+			var tt = SmoSelection.noteSelection(layout.score, 0, 0, 0, 3);
+			SmoUndoable.crescendo(ft, tt, undo);
+			layout.render();
+			return timeTest();
+		}
+		var keySigTest2 = () => {
 			subTitle('key sig to Bb');
-			var selection = SmoSelection.measureSelection(score,0,2);
-			SmoUndoable.addKeySignature(score,selection,'Bb',undo);
+			var selection = SmoSelection.measureSelection(score, 0, 2);
+			SmoUndoable.addKeySignature(score, selection, 'Bb', undo);
 			keys.render();
 			return timeTest();
 		}
-		
+
 		var addInstrument = () => {
 			subTitle('add instrument');
-			SmoUndoable.addInstrument(score,undo);
+			SmoUndoable.addInstrument(score, {
+				instrumentInfo: {
+					instrumentName: 'Treble Instrument',
+					keyOffset: 0,
+					clef: 'treble'
+				}
+			},
+				undo);
 			keys.layout.render();
 			keys.tracker.updateMap();
 			return timeTest();
 		}
-		var keySigTest3= () => {
+		var keySigTest3 = () => {
 			subTitle('key sig change to f#');
-			var selection = SmoSelection.measureSelection(score,0,1);
-			SmoUndoable.addKeySignature(score,selection,'C#',undo);
-			selection = SmoSelection.measureSelection(score,0,2);
-			SmoUndoable.addKeySignature(score,selection,'Cb',undo);
-			keys.render();			
+			var selection = SmoSelection.measureSelection(score, 0, 1);
+			SmoUndoable.addKeySignature(score, selection, 'C#', undo);
+			selection = SmoSelection.measureSelection(score, 0, 2);
+			SmoUndoable.addKeySignature(score, selection, 'Cb', undo);
+			keys.render();
 			return timeTest();
-		}             
-        return drawDefaults().then(changePitch).then(crescendoTest).then(changePitch2)
-		    .then(keySigTest).then(keySigTest2).then(addInstrument)
-		    .then(keySigTest3).then(undoTest).then(undoTest).then(undoTest).then(undoTest).then(undoTest).then(undoTest)
-			.then(undoTest).then(undoTest).then(undoTest).then(undoTest)
-			then(signalComplete);
-    }
+		}
+		return drawDefaults().then(changePitch).then(crescendoTest).then(changePitch2)
+		.then(keySigTest).then(keySigTest2).then(addInstrument)
+		.then(keySigTest3).then(undoTest).then(undoTest).then(undoTest).then(undoTest).then(undoTest).then(undoTest)
+		.then(undoTest).then(undoTest).then(undoTest).then(undoTest)
+		then(signalComplete);
+	}
 }
 ;
 class TimeSignatureTest {
--- build/smoosic.js
+++ build/smoosic.js
@@ -3910,6 +3910,18 @@ class SmoUndoable {
 		var measureIndex = selection.selector.measure;		
 		score.deleteMeasure(measureIndex);
 	}
+	static addInstrument(score, parameters,undoBuffer) {
+        undoBuffer.addBuffer('add instrument', 'score', null, score);
+		SmoOperation.addInstrument(score,parameters);
+	}
+	static removeInstrument(score, index,undoBuffer) {
+        undoBuffer.addBuffer('remove instrument', 'score', null, score);
+		SmoOperation.removeInstrument(score,index);
+	}
+	static changeInstrument(score,instrument, selections,undoBuffer) {		
+		undoBuffer.addBuffer('changeInstrument', 'staff', selections[0].selector, score);
+		SmoOperation.changeInstrument(score,instrument,selections);
+	}
 }
 ;
 
@@ -5767,7 +5779,7 @@ class suiEditor {
 
     unmakeTuplet(keyEvent) {
         this._singleSelectionOperation('unmakeTuplet');
-    }
+    }	
 
     addRemoveArticulation(keyEvent) {
         if (this.tracker.selections.length < 1)
@@ -5807,328 +5819,404 @@ class suiEditor {
 }
 ;
 class suiMenuBase {
-    constructor(params) {
-        Vex.Merge(this, params);
-    }
+	constructor(params) {
+		Vex.Merge(this, params);
+	}
 
-    complete() {
-        $('body').trigger('menuDismiss');
-    }
+	complete() {
+		$('body').trigger('menuDismiss');
+	}
 }
 
 class suiMenuManager {
-    constructor(params) {
-        Vex.Merge(this, suiMenuManager.defaults);
-        Vex.Merge(this, params);
-        this.bound = false;
-    }
+	constructor(params) {
+		Vex.Merge(this, suiMenuManager.defaults);
+		Vex.Merge(this, params);
+		this.bound = false;
+	}
 
-    static get defaults() {
-        return {
-            menuBind: suiMenuManager.menuKeyBindingDefaults,
-            menuContainer: '.menuContainer'
-        };
-    }
+	static get defaults() {
+		return {
+			menuBind: suiMenuManager.menuKeyBindingDefaults,
+			menuContainer: '.menuContainer'
+		};
+	}
 
-    // ### Description:
-    // slash ('/') menu key bindings.  The slash key followed by another key brings up
-    // a menu.
-    static get menuKeyBindingDefaults() {
-        return [{
-                event: "keydown",
-                key: "k",
-                ctrlKey: false,
-                altKey: false,
-                shiftKey: false,
-                action: "suiKeySignatureMenu"
-            }, {
-                event: "keydown",
-                key: "e",
-                ctrlKey: false,
-                altKey: false,
-                shiftKey: false,
-                action: "suiStaffModifierMenu"
-            },
-			{
-                event: "keydown",
-                key: "d",
-                ctrlKey: false,
-                altKey: false,
-                shiftKey: false,
-                action: "SuiDynamicsMenu"
-            }
-        ];
-    }
+	// ### Description:
+	// slash ('/') menu key bindings.  The slash key followed by another key brings up
+	// a menu.
+	static get menuKeyBindingDefaults() {
+		return [{
+				event: "keydown",
+				key: "k",
+				ctrlKey: false,
+				altKey: false,
+				shiftKey: false,
+				action: "suiKeySignatureMenu"
+			}, {
+				event: "keydown",
+				key: "e",
+				ctrlKey: false,
+				altKey: false,
+				shiftKey: false,
+				action: "suiStaffModifierMenu"
+			}, {
+				event: "keydown",
+				key: "d",
+				ctrlKey: false,
+				altKey: false,
+				shiftKey: false,
+				action: "SuiDynamicsMenu"
+			}, {
+				event: "keydown",
+				key: "i",
+				ctrlKey: false,
+				altKey: false,
+				shiftKey: false,
+				action: "SuiAddInstrumentsMenu"
+			}
 
-    unattach() {
-        window.removeEventListener("keydown", this.keydownHandler, true);
-        $('body').removeClass('modal');
-        $(this.menuContainer).html('');
-        $('body').off('dismissMenu');
-        this.bound = false;
-    }
+		];
+	}
 
-    attach(el) {
-        var b = htmlHelpers.buildDom();
+	unattach() {
+		window.removeEventListener("keydown", this.keydownHandler, true);
+		$('body').removeClass('modal');
+		$(this.menuContainer).html('');
+		$('body').off('dismissMenu');
+		this.bound = false;
+	}
 
-        $(this.menuContainer).html('');
-        $(this.menuContainer).attr('z-index', '12');
-        var b = htmlHelpers.buildDom;
-        var r = b('ul').classes('menuElement').attr('size', this.menu.menuItems.length)
-            .css('left', '' + this.menuPosition.x + 'px')
-            .css('top', '' + this.menuPosition.y + 'px')
-            .css('height', '' + this.menu.menuItems.length * 35 + 'px');
-        this.menu.menuItems.forEach((item) => {
-            r.append(
-                b('li').classes('menuOption').append(
-                    b('button')
-                    .text(item.text).attr('data-value', item.value)
-                    .append(
-                        b('span').classes('icon icon-' + item.icon))));
-        });
-        $(this.menuContainer).append(r.dom());
-        $('body').addClass('modal');
-        this.bindEvents();
-    }
-    slashMenuMode() {
-        var self = this;
-        this.bindEvents();
-        this.closeMenuPromise = new Promise((resolve, reject) => {
-                $('body').off('menuDismiss').on('menuDismiss', function () {
-                    self.unattach();
-                    resolve();
-                });
+	attach(el) {
+		var b = htmlHelpers.buildDom();
 
-            });
-        return this.closeMenuPromise;
-    }
+		$(this.menuContainer).html('');
+		$(this.menuContainer).attr('z-index', '12');
+		var b = htmlHelpers.buildDom;
+		var r = b('ul').classes('menuElement').attr('size', this.menu.menuItems.length)
+			.css('left', '' + this.menuPosition.x + 'px')
+			.css('top', '' + this.menuPosition.y + 'px')
+			.css('height', '' + this.menu.menuItems.length * 35 + 'px');
+		this.menu.menuItems.forEach((item) => {
+			r.append(
+				b('li').classes('menuOption').append(
+					b('button')
+					.text(item.text).attr('data-value', item.value)
+					.append(
+						b('span').classes('icon icon-' + item.icon))));
+		});
+		$(this.menuContainer).append(r.dom());
+		$('body').addClass('modal');
+		this.bindEvents();
+	}
+	slashMenuMode() {
+		var self = this;
+		this.bindEvents();
+		this.closeMenuPromise = new Promise((resolve, reject) => {
+				$('body').off('menuDismiss').on('menuDismiss', function () {
+					self.unattach();
+					resolve();
+				});
 
-    handleKeydown(event) {
-        console.log("KeyboardEvent: key='" + event.key + "' | code='" +
-            event.code + "'"
-             + " shift='" + event.shiftKey + "' control='" + event.ctrlKey + "'" + " alt='" + event.altKey + "'");
-        if (['Tab', 'Enter'].indexOf(event.code) >= 0) {
-            return;
-        }
+			});
+		return this.closeMenuPromise;
+	}
 
-        event.preventDefault();
+	handleKeydown(event) {
+		console.log("KeyboardEvent: key='" + event.key + "' | code='" +
+			event.code + "'"
+			 + " shift='" + event.shiftKey + "' control='" + event.ctrlKey + "'" + " alt='" + event.altKey + "'");
+		if (['Tab', 'Enter'].indexOf(event.code) >= 0) {
+			return;
+		}
 
-        if (event.code === 'Escape') {
-            $('body').trigger('menuDismiss');
-        }
-        if (this.menu) {
-            this.menu.keydown(event);			
-        }
-        if (this.tracker.selections.length == 0) {
-            this.unattach();
-            return;
-        }
-        this.menuPosition = this.tracker.selections[0].box;
-        var binding = this.menuBind.find((ev) => {
-                return ev.key === event.key
-            });
-        if (!binding) {
-            return;
-        }
-        var ctor = eval(binding.action);
-        this.menu = new ctor({
-                position: this.menuPosition,
-                tracker: this.tracker,
-                editor: this.editor,
-                score: this.score
-            });
-        this.attach(this.menuContainer);
-    }
+		event.preventDefault();
 
-    bindEvents() {
-        var self = this;
+		if (event.code === 'Escape') {
+			$('body').trigger('menuDismiss');
+		}
+		if (this.menu) {
+			this.menu.keydown(event);
+		}
+		if (this.tracker.selections.length == 0) {
+			this.unattach();
+			return;
+		}
+		this.menuPosition = this.tracker.selections[0].box;
+		var binding = this.menuBind.find((ev) => {
+				return ev.key === event.key
+			});
+		if (!binding) {
+			return;
+		}
+		var ctor = eval(binding.action);
+		this.menu = new ctor({
+				position: this.menuPosition,
+				tracker: this.tracker,
+				editor: this.editor,
+				score: this.score
+			});
+		this.attach(this.menuContainer);
+	}
 
-        if (!this.bound) {
-            this.keydownHandler = this.handleKeydown.bind(this);
+	bindEvents() {
+		var self = this;
 
-            window.addEventListener("keydown", this.keydownHandler, true);
-            this.bound = true;
-        }
-        $(this.menuContainer).find('button').off('click').on('click', function (ev) {
-            self.menu.selection(ev);
-        });
-    }
+		if (!this.bound) {
+			this.keydownHandler = this.handleKeydown.bind(this);
+
+			window.addEventListener("keydown", this.keydownHandler, true);
+			this.bound = true;
+		}
+		$(this.menuContainer).find('button').off('click').on('click', function (ev) {
+			self.menu.selection(ev);
+		});
+	}
 }
 
 class SuiDynamicsMenu extends suiMenuBase {
-	 constructor(params) {
-        params = (params ? params : {});
-        Vex.Merge(params, SuiDynamicsMenu.defaults);
-        super(params);
-    }
+	constructor(params) {
+		params = (params ? params : {});
+		Vex.Merge(params, SuiDynamicsMenu.defaults);
+		super(params);
+	}
 	static get defaults() {
-		return {menuItems: [ 
-		{icon: 'pianissimo',
-                    text: 'Pianissimo',
-                    value: 'pp'
-                },
-		{icon: 'piano',
-                    text: 'Piano',
-                    value: 'p'
-                },
-		{icon: 'mezzopiano',
-                    text: 'Mezzo-piano',
-                    value: 'mp'
-                },
-		{icon: 'mezzoforte',
-                    text: 'Mezzo-forte',
-                    value: 'mf'
-        },
-		{icon: 'forte',
-                    text: 'Forte',
-                    value: 'f'
-        },
-		{icon: 'fortissimo',
-                    text: 'Fortissimo',
-                    value: 'ff'
-        },
-		{icon: 'sfz',
-                    text: 'sfortzando',
-                    value: 'sfz'
-        }
-		]				
-	};
+		return {
+			menuItems: [{
+					icon: 'pianissimo',
+					text: 'Pianissimo',
+					value: 'pp'
+				}, {
+					icon: 'piano',
+					text: 'Piano',
+					value: 'p'
+				}, {
+					icon: 'mezzopiano',
+					text: 'Mezzo-piano',
+					value: 'mp'
+				}, {
+					icon: 'mezzoforte',
+					text: 'Mezzo-forte',
+					value: 'mf'
+				}, {
+					icon: 'forte',
+					text: 'Forte',
+					value: 'f'
+				}, {
+					icon: 'fortissimo',
+					text: 'Fortissimo',
+					value: 'ff'
+				}, {
+					icon: 'sfz',
+					text: 'sfortzando',
+					value: 'sfz'
+				}
+			]
+		};
 	}
 	selection(ev) {
-        var text = $(ev.currentTarget).attr('data-value');
+		var text = $(ev.currentTarget).attr('data-value');
 
-        var ft = this.tracker.getExtremeSelection(-1);
+		var ft = this.tracker.getExtremeSelection(-1);
 		if (!ft || !ft.note) {
 			return;
 		}
 
-        SmoUndoable.addDynamic(ft,new SmoDynamicText({selector:ft.selector,text:text,yOffsetLine:11,fontSize:38}),this.editor.undoBuffer);
-        this.complete();
-    }
-    keydown(ev) {}
+		SmoUndoable.addDynamic(ft, new SmoDynamicText({
+				selector: ft.selector,
+				text: text,
+				yOffsetLine: 11,
+				fontSize: 38
+			}), this.editor.undoBuffer);
+		this.complete();
+	}
+	keydown(ev) {}
 }
 
 class suiKeySignatureMenu extends suiMenuBase {
 
-    constructor(params) {
-        params = (params ? params : {});
-        Vex.Merge(params, suiKeySignatureMenu.defaults);
-        super(params);
-    }
-    static get defaults() {
-        return {
-            menuItems: [{
-                    icon: 'key-sig-c',
-                    text: 'C Major',
-                    value: 'C'
-                }, {
-                    icon: 'key-sig-f',
-                    text: 'F Major',
-                    value: 'F'
-                }, {
-                    icon: 'key-sig-g',
-                    text: 'G Major',
-                    value: 'G'
-                }, {
-                    icon: 'key-sig-bb',
-                    text: 'Bb Major',
-                    value: 'Bb'
-                }, {
-                    icon: 'key-sig-d',
-                    text: 'D Major',
-                    value: 'D'
-                }, {
-                    icon: 'key-sig-eb',
-                    text: 'Eb Major',
-                    value: 'Eb'
-                }, {
-                    icon: 'key-sig-a',
-                    text: 'A Major',
-                    value: 'A'
-                }, {
-                    icon: 'key-sig-ab',
-                    text: 'Ab Major',
-                    value: 'Ab'
-                }, {
-                    icon: 'key-sig-e',
-                    text: 'E Major',
-                    value: 'E'
-                }, {
-                    icon: 'key-sig-bd',
-                    text: 'Db Major',
-                    value: 'Db'
-                }, {
-                    icon: 'key-sig-b',
-                    text: 'B Major',
-                    value: 'B'
-                }, {
-                    icon: 'key-sig-fs',
-                    text: 'F# Major',
-                    value: 'F#'
-                }, {
-                    icon: 'key-sig-cs',
-                    text: 'C# Major',
-                    value: 'C#'
-                }
-            ],
-            menuContainer: '.menuContainer'
-        };
-    }
-    selection(ev) {
-        var keySig = $(ev.currentTarget).attr('data-value');
+	constructor(params) {
+		params = (params ? params : {});
+		Vex.Merge(params, suiKeySignatureMenu.defaults);
+		super(params);
+	}
+	static get defaults() {
+		return {
+			menuItems: [{
+					icon: 'key-sig-c',
+					text: 'C Major',
+					value: 'C'
+				}, {
+					icon: 'key-sig-f',
+					text: 'F Major',
+					value: 'F'
+				}, {
+					icon: 'key-sig-g',
+					text: 'G Major',
+					value: 'G'
+				}, {
+					icon: 'key-sig-bb',
+					text: 'Bb Major',
+					value: 'Bb'
+				}, {
+					icon: 'key-sig-d',
+					text: 'D Major',
+					value: 'D'
+				}, {
+					icon: 'key-sig-eb',
+					text: 'Eb Major',
+					value: 'Eb'
+				}, {
+					icon: 'key-sig-a',
+					text: 'A Major',
+					value: 'A'
+				}, {
+					icon: 'key-sig-ab',
+					text: 'Ab Major',
+					value: 'Ab'
+				}, {
+					icon: 'key-sig-e',
+					text: 'E Major',
+					value: 'E'
+				}, {
+					icon: 'key-sig-bd',
+					text: 'Db Major',
+					value: 'Db'
+				}, {
+					icon: 'key-sig-b',
+					text: 'B Major',
+					value: 'B'
+				}, {
+					icon: 'key-sig-fs',
+					text: 'F# Major',
+					value: 'F#'
+				}, {
+					icon: 'key-sig-cs',
+					text: 'C# Major',
+					value: 'C#'
+				}
+			],
+			menuContainer: '.menuContainer'
+		};
+	}
+	selection(ev) {
+		var keySig = $(ev.currentTarget).attr('data-value');
 		var changed = [];
-        this.tracker.selections.forEach((sel) => {
+		this.tracker.selections.forEach((sel) => {
 			if (changed.indexOf(sel.selector.measure) === -1) {
 				changed.push(sel.selector.measure);
-				SmoUndoable.addKeySignature(this.score,sel,keySig,this.editor.undoBuffer);
+				SmoUndoable.addKeySignature(this.score, sel, keySig, this.editor.undoBuffer);
 			}
-        });
-        this.complete();
-    }
-    keydown(ev) {}
+		});
+		this.complete();
+	}
+	keydown(ev) {}
 
 }
 
 class suiStaffModifierMenu extends suiMenuBase {
 
-    constructor(params) {
-        params = (params ? params : {});
-        Vex.Merge(params, suiStaffModifierMenu.defaults);
-        super(params);
-    }
-    static get defaults() {
-        return {
-            menuItems: [{
-                    icon: 'cresc',
-                    text: 'Crescendo',
-                    value: 'crescendo'
-                }, {
-                    icon: 'decresc',
-                    text: 'Decrescendo',
-                    value: 'decrescendo'
-                }, {
-                    icon: 'slur',
-                    text: 'Slur/Tie',
-                    value: 'slur'
-                }
-            ],
-            menuContainer: '.menuContainer'
-        };
-    }
-    selection(ev) {
-        var op = $(ev.currentTarget).attr('data-value');
+	constructor(params) {
+		params = (params ? params : {});
+		Vex.Merge(params, suiStaffModifierMenu.defaults);
+		super(params);
+	}
+	static get defaults() {
+		return {
+			menuItems: [{
+					icon: 'cresc',
+					text: 'Crescendo',
+					value: 'crescendo'
+				}, {
+					icon: 'decresc',
+					text: 'Decrescendo',
+					value: 'decrescendo'
+				}, {
+					icon: 'slur',
+					text: 'Slur/Tie',
+					value: 'slur'
+				}
+			],
+			menuContainer: '.menuContainer'
+		};
+	}
+	selection(ev) {
+		var op = $(ev.currentTarget).attr('data-value');
 
-        var ft = this.tracker.getExtremeSelection(-1);
-        var tt = this.tracker.getExtremeSelection(1);
-        if (SmoSelector.sameNote(ft.selector, tt.selector)) {
-            this.complete();
-            return;
-        }
+		var ft = this.tracker.getExtremeSelection(-1);
+		var tt = this.tracker.getExtremeSelection(1);
+		if (SmoSelector.sameNote(ft.selector, tt.selector)) {
+			this.complete();
+			return;
+		}
 
-        SmoUndoable[op](ft, tt,this.editor.undoBuffer);
-        this.complete();
-    }
-    keydown(ev) {}
+		SmoUndoable[op](ft, tt, this.editor.undoBuffer);
+		this.complete();
+	}
+	keydown(ev) {}
+}
+
+class SuiAddInstrumentsMenu extends suiMenuBase {
+	constructor(params) {
+		params = (params ? params : {});
+		Vex.Merge(params, SuiAddInstrumentsMenu.defaults);
+		super(params);
+	}
+	static get defaults() {
+		return {
+			menuItems: [{
+					icon: 'treble',
+					text: 'Treble Clef Instrument',
+					value: 'trebleInstrument'
+				}, {
+					icon: 'bass',
+					text: 'Bass Clef Instrument',
+					value: 'bassInstrument'
+				}, {
+					icon: 'alto',
+					text: 'Alto Clef Instrument',
+					value: 'altoInstrument'
+				}
+			],
+			menuContainer: '.menuContainer'
+		};
+	}
+
+	static get instrumentMap() {
+		return {
+			'trebleInstrument': {
+				instrumentInfo: {
+					instrumentName: 'Treble Instrument',
+					keyOffset: 0,
+					clef: 'treble'
+				}
+			},
+			'bassInstrument': {
+				instrumentInfo: {
+					instrumentName: 'Bass Instrument',
+					keyOffset: 0,
+					clef: 'bass'
+				}
+			},
+			'altoInstrument': {
+				instrumentInfo: {
+					instrumentName: 'Treble Instrument',
+					keyOffset: 0,
+					clef: 'treble'
+				}
+			}
+		}
+
+	}
+	selection(ev) {
+		var op = $(ev.currentTarget).attr('data-value');
+		var instrument = SuiAddInstrumentsMenu.instrumentMap[op];
+
+		SmoUndoable.addInstrument(this.score, instrument, this.editor.undoBuffer);
+		this.complete();
+	}
+	keydown(ev) {}
+
 }
 ;
 // ## utController
