a36fdcbda67c56d16a38f0232328a9a045875c23
Aaron <aarondnewman@gmail.com>
2019-10-15 19:13:10 -0500

    more adjuster
    sync build

--- src/render/sui/adjuster.js
+++ src/render/sui/adjuster.js
@@ -2,7 +2,175 @@
 // ## suiAdjuster
 // Perform adjustments on the score based on the rendered components so we can re-render it more legibly.
 class suiAdjuster {	
+	
+	static estimateMusicWidth(smoMeasure) {
+		var width = 0;
+		var tm = smoMeasure.tickmap();
+		smoMeasure.voices.forEach((voice) => {
+			var tickIndex = 0;
+			voice.notes.forEach((note) => {
+				width += vexGlyph.dimensions.noteHead.width + vexGlyph.dimensions.noteHead.spacingRight;
+				width += vexGlyph.dimensions.dot.width * note.dots + vexGlyph.dimensions.dot.spacingRight * note.dots;
+				note.pitches.forEach((pitch) => {
+					var declared = tm.getActiveAccidental(pitch, tickIndex, smoMeasure.keySignature);
+
+					if (pitch.accidental != declared || pitch.cautionary) {
+						width += vexGlyph.accidental(pitch.accidental).width;
+					}
+				});
+				tickIndex += 1;
+			});
+		});
+		return width;
+	}
+
+	static estimateStartSymbolWidth(smoMeasure) {
+		var width = 0;
+		if (smoMeasure.forceKeySignature) {
+			if ( smoMeasure.canceledKeySignature) {
+			    width += vexGlyph.keySignatureLength(smoMeasure.canceledKeySignature);
+			}			
+            width += vexGlyph.keySignatureLength(smoMeasure.keySignature);
+		}
+		if (smoMeasure.forceClef) {
+			width += vexGlyph.clef(smoMeasure.clef).width + vexGlyph.clef(smoMeasure.clef).spacingRight;
+		}
+		if (smoMeasure.forceTimeSignature) {
+			width += vexGlyph.dimensions.timeSignature.width + vexGlyph.dimensions.timeSignature.spacingRight;
+		}
+		var starts = smoMeasure.getStartBarline();
+		if (starts) {
+			width += vexGlyph.barWidth(starts);
+		}
+		return width;
+	}
+	
+	static estimateEndSymbolWidth(smoMeasure) {
+		var width = 0;
+		var ends  = smoMeasure.getEndBarline();
+		if (ends) {
+			width += vexGlyph.barWidth(ends);
+		}
+		return width;
+	}
+	
+	// ### justifyWidths
+	// After we adjust widths so each staff has enough room, evenly distribute the remainder widths to the measures.
+	static justifyWidths(score,renderer,pageSize) {
+		var context = renderer.getContext();
+		var svg = context.svg;
 		
+		if (suiSimpleLayout.debugLayout) {
+			$(context.svg).find('g.measure-adjust-dbg').remove();
+		}
+		var topStaff = score.staves[0];
+		var maxLine = topStaff.measures[topStaff.measures.length - 1].lineIndex - 1;
+		for (var i = 0; i <= maxLine; ++i) {
+			var systemIndex = 0;
+
+			score.staves.forEach((staff) => {
+				var measures = staff.measures.filter((mm) => {
+						return mm.lineIndex === i
+					});
+				if (measures.length > 0) {
+					var width = measures.map((mm) => {
+							return mm.staffWidth;
+						}).reduce((a, b) => {
+							return a + b
+						});
+					width += measures[0].staffX + score.layout.leftMargin;
+					var just = Math.round((pageSize - width) / measures.length) - 1;
+					if (just > 0) {
+						var accum = 0;
+						measures.forEach((mm) => {
+							mm.staffWidth += just;
+							mm.staffX += accum;
+							accum += just;
+							if (suiSimpleLayout.debugLayout) {
+								var dbgBox = svgHelpers.boxPoints(
+										mm.staffX, mm.staffY, mm.staffWidth, mm.logicalBox.height);
+								svgHelpers.debugBox(svg, dbgBox, 'measure-adjust-dbg', 10);
+							}
+						});
+					}
+				}
+			});
+		}
+	}
+
+	static _spaceNotes(svg,smoMeasure) {
+		var g = svg.getElementById(smoMeasure.attrs.id);
+		var notes = Array.from(g.getElementsByClassName('vf-stavenote'));
+		var acc = 0;
+		for (var i = 1; i < notes.length; ++i) {
+			var b1 = notes[i - 1].getBBox();
+			var b2 = notes[i].getBBox();
+			var dif = b2.x - (b1.x + b1.width);
+			if (dif < 10) {
+				acc += 10 - dif;
+			}
+		}
+		smoMeasure.logicalBox.width += acc;
+	}
+	
+    // ### adjustWidths
+	// Set the width of each measure in a system to the max width for that column so the measures are aligned.
+	static adjustWidths(score,renderer) {
+		var topStaff = score.staves[0];
+		var maxLine = topStaff.measures[topStaff.measures.length - 1].lineIndex;
+		var svg = renderer.getContext().svg;
+		if (suiSimpleLayout.debugLayout) {
+			$(renderer.getContext().svg).find('g.measure-adjust-dbg').remove();
+		}
+
+		for (var i = 0; i <= maxLine; ++i) {
+			var systemIndex = 0;
+			while (true) {
+				var measures = [];
+				score.staves.forEach((staff) => {
+					var ix = staff.measures.findIndex((x) => {
+							return x.lineIndex === i && x.measureNumber.systemIndex === systemIndex;
+						});
+					if (ix >= 0) {
+						measures.push(staff.measures[ix]);
+					}
+				});
+				// Make sure each note head is not squishing
+				measures.forEach((mm) => {suiAdjuster._spaceNotes(svg,mm);});
+
+				if (measures.length) {
+					var widest = measures.map((x) => {
+							return x.logicalBox.width;
+						}).reduce((a, w) => {
+							return a > w ? a : w;
+						});
+					measures.forEach((measure) => {
+						measure.staffWidth = widest;
+						measure.changed = true;
+					});
+				}
+				if (!measures.length)
+					break;
+				systemIndex += 1;
+			}
+		}
+
+		score.staves.forEach((staff) => {
+			var last = null;
+			staff.measures.forEach((measure) => {
+				if (last && measure.measureNumber.systemIndex > 0) {
+					measure.staffX = last.staffX + last.staffWidth;
+				}
+				if (suiSimpleLayout.debugLayout) {
+					var dbgBox = svgHelpers.boxPoints(measure.staffX, measure.staffY, measure.staffWidth, measure.logicalBox.height);
+					svgHelpers.debugBox(svg, dbgBox, 'measure-adjust-dbg', 10);
+				}
+				last = measure;
+			});
+		});
+
+	}
+	
 	static _minMaxYModifier(staff,minY,maxY) {
 		staff.modifiers.forEach((modifier) => {
 			minY = modifier.logicalBox.y < minY ? modifier.logicalBox.y : minY;
--- src/render/sui/layout.js
+++ src/render/sui/layout.js
@@ -188,171 +188,8 @@ class suiSimpleLayout {
 		this._renderModifiers(startSelection.staff, system);
 	}
 
-	_spaceNotes(smoMeasure) {
-		var g = this.context.svg.getElementById(smoMeasure.attrs.id);
-		var notes = Array.from(g.getElementsByClassName('vf-stavenote'));
-		var acc = 0;
-		for (var i = 1; i < notes.length; ++i) {
-			var b1 = notes[i - 1].getBBox();
-			var b2 = notes[i].getBBox();
-			var dif = b2.x - (b1.x + b1.width);
-			if (dif < 10) {
-				acc += 10 - dif;
-			}
-		}
-		smoMeasure.logicalBox.width += acc;
-	}
-
-	// ### justifyWidths
-	// After we adjust widths so each staff has enough room, evenly distribute the remainder widths to the measures.
-	justifyWidths() {
-		var svg = this.context.svg;
-		if (suiSimpleLayout.debugLayout) {
-			$(this.context.svg).find('g.measure-adjust-dbg').remove();
-		}
-		var topStaff = this.score.staves[0];
-		var maxLine = topStaff.measures[topStaff.measures.length - 1].lineIndex - 1;
-		for (var i = 0; i <= maxLine; ++i) {
-			var systemIndex = 0;
-
-			this.score.staves.forEach((staff) => {
-				var measures = staff.measures.filter((mm) => {
-						return mm.lineIndex === i
-					});
-				if (measures.length > 0) {
-					var width = measures.map((mm) => {
-							return mm.staffWidth;
-						}).reduce((a, b) => {
-							return a + b
-						});
-					width += measures[0].staffX + this.score.layout.leftMargin;
-					var just = Math.round(((this.pageMarginWidth / this.svgScale) - width) / measures.length) - 1;
-					if (just > 0) {
-						var accum = 0;
-						measures.forEach((mm) => {
-							mm.staffWidth += just;
-							mm.staffX += accum;
-							accum += just;
-							if (suiSimpleLayout.debugLayout) {
-								var dbgBox = svgHelpers.boxPoints(
-										mm.staffX, mm.staffY, mm.staffWidth, mm.logicalBox.height);
-								svgHelpers.debugBox(svg, dbgBox, 'measure-adjust-dbg', 10);
-							}
-						});
-					}
-				}
-			});
-		}
-	}
-
-	// ### adjustWidths
-	// Set the width of each measure in a system to the max width for that column so the measures are aligned.
-	adjustWidths() {
-		var topStaff = this.score.staves[0];
-		var maxLine = topStaff.measures[topStaff.measures.length - 1].lineIndex;
-		var svg = this.context.svg;
-		if (suiSimpleLayout.debugLayout) {
-			$(this.renderer.getContext().svg).find('g.measure-adjust-dbg').remove();
-		}
-
-		for (var i = 0; i <= maxLine; ++i) {
-			var systemIndex = 0;
-			while (true) {
-				var measures = [];
-				this.score.staves.forEach((staff) => {
-					var ix = staff.measures.findIndex((x) => {
-							return x.lineIndex === i && x.measureNumber.systemIndex === systemIndex;
-						});
-					if (ix >= 0) {
-						measures.push(staff.measures[ix]);
-					}
-				});
-				// Make sure each note head is not squishing
-				measures.forEach((mm) => {this._spaceNotes(mm);});
-
-				if (measures.length) {
-					var widest = measures.map((x) => {
-							return x.logicalBox.width;
-						}).reduce((a, w) => {
-							return a > w ? a : w;
-						});
-					measures.forEach((measure) => {
-						measure.staffWidth = widest;
-						measure.changed = true;
-					});
-				}
-				if (!measures.length)
-					break;
-				systemIndex += 1;
-			}
-		}
-
-		this.score.staves.forEach((staff) => {
-			var last = null;
-			staff.measures.forEach((measure) => {
-				if (last && measure.measureNumber.systemIndex > 0) {
-					measure.staffX = last.staffX + last.staffWidth;
-				}
-				if (suiSimpleLayout.debugLayout) {
-					var dbgBox = svgHelpers.boxPoints(measure.staffX, measure.staffY, measure.staffWidth, measure.logicalBox.height);
-					svgHelpers.debugBox(svg, dbgBox, 'measure-adjust-dbg', 10);
-				}
-				last = measure;
-			});
-		});
-
-	}
-
-	estimateMusicWidth(smoMeasure) {
-		var width = 0;
-		var tm = smoMeasure.tickmap();
-		smoMeasure.voices.forEach((voice) => {
-			var tickIndex = 0;
-			voice.notes.forEach((note) => {
-				width += vexGlyph.dimensions.noteHead.width + vexGlyph.dimensions.noteHead.spacingRight;
-				width += vexGlyph.dimensions.dot.width * note.dots + vexGlyph.dimensions.dot.spacingRight * note.dots;
-				note.pitches.forEach((pitch) => {
-					var declared = tm.getActiveAccidental(pitch, tickIndex, smoMeasure.keySignature);
-
-					if (pitch.accidental != declared || pitch.cautionary) {
-						width += vexGlyph.accidental(pitch.accidental).width;
-					}
-				});
-				tickIndex += 1;
-			});
-		});
-		return width;
-	}
-
-	estimateStartSymbolWidth(smoMeasure) {
-		var width = 0;
-		if (smoMeasure.forceKeySignature) {
-			if ( smoMeasure.canceledKeySignature) {
-			    width += vexGlyph.keySignatureLength(smoMeasure.canceledKeySignature);
-			}			
-            width += vexGlyph.keySignatureLength(smoMeasure.keySignature);
-		}
-		if (smoMeasure.forceClef) {
-			width += vexGlyph.clef(smoMeasure.clef).width + vexGlyph.clef(smoMeasure.clef).spacingRight;
-		}
-		if (smoMeasure.forceTimeSignature) {
-			width += vexGlyph.dimensions.timeSignature.width + vexGlyph.dimensions.timeSignature.spacingRight;
-		}
-		var starts = smoMeasure.getStartBarline();
-		if (starts) {
-			width += vexGlyph.barWidth(starts);
-		}
-		return width;
-	}
 	
-	estimateEndSymbolWidth(smoMeasure) {
-		var width = 0;
-		var ends  = smoMeasure.getEndBarline();
-		if (ends) {
-			width += vexGlyph.barWidth(ends);
-		}
-		return width;
-	}
+	
 
 
 	// ### unrenderMeasure
@@ -491,7 +328,7 @@ class suiSimpleLayout {
 		for (var i=0;i<10;++i) {
 			params.useX=true;
 			console.log('adjust width '+i);
-		    this.adjustWidths();
+		    suiAdjuster.adjustWidths(this.score,this.renderer);
 			this.layout(params);
 			
 			// If useX is still true, there was no wrapping and we
@@ -501,7 +338,7 @@ class suiSimpleLayout {
 			}
 			params.useX = true;
 		}
-		this.justifyWidths();
+		suiAdjuster.justifyWidths(this.score,this.renderer,this.pageMarginWidth / this.svgScale);
 		suiAdjuster.adjustHeight(this.score,this.renderer);
 		params.useY=true;
 		this.layout(params);
@@ -594,9 +431,9 @@ class suiSimpleLayout {
     				measure.staffX = staffBox.x + staffBox.width;
 	
                 	// Calculate the existing staff width, based on the notes and what we expect to be rendered.
-					measure.staffWidth = this.estimateMusicWidth(measure);
-					measure.adjX = this.estimateStartSymbolWidth(measure);
-					measure.adjRight = this.estimateEndSymbolWidth(measure);
+					measure.staffWidth = suiAdjuster.estimateMusicWidth(measure);
+					measure.adjX = suiAdjuster.estimateStartSymbolWidth(measure);
+					measure.adjRight = suiAdjuster.estimateEndSymbolWidth(measure);
 					measure.staffWidth = measure.staffWidth  + measure.adjX + measure.adjRight;
 				}
 
@@ -634,9 +471,9 @@ class suiSimpleLayout {
 					// If we have wrapped lines, calculate the beginning stuff again.
 					this.calculateBeginningSymbols(systemIndex, measure, clefLast, keySigLast, timeSigLast);
 					if (!useAdjustedX) {
-						measure.staffWidth = this.estimateMusicWidth(measure);
-						measure.adjX = this.estimateStartSymbolWidth(measure);
-						measure.adjRight = this.estimateEndSymbolWidth(measure);
+						measure.staffWidth = suiAdjuster.estimateMusicWidth(measure);
+						measure.adjX = suiAdjuster.estimateStartSymbolWidth(measure);
+						measure.adjRight = suiAdjuster.estimateEndSymbolWidth(measure);
 						measure.staffWidth = measure.staffWidth + measure.adjX + measure.adjRight;
 					}
 				}
