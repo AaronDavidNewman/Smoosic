0a9e54cc42dcdffce0e33b47728bba420d0cd59a
AaronDavidNewman <anewman@air.org>
2019-10-30 21:57:44 -0500

    Update noteModifiers.js

--- src/smo/data/noteModifiers.js
+++ src/smo/data/noteModifiers.js
@@ -1,173 +1,218 @@
 
 class SmoNoteModifierBase {
-    constructor(ctor) {
-        this.ctor = ctor;
-    }
-    static deserialize(jsonObj) {
-        var ctor = eval(jsonObj.ctor);
-        var rv = new ctor(jsonObj);
-        rv.attrs.id = jsonObj.attrs.id;
-        rv.attrs.type = jsonObj.attrs.type;
+	constructor(ctor) {
+		this.ctor = ctor;
+	}
+	static deserialize(jsonObj) {
+		var ctor = eval(jsonObj.ctor);
+		var rv = new ctor(jsonObj);
+		rv.attrs.id = jsonObj.attrs.id;
+		rv.attrs.type = jsonObj.attrs.type;
 		return rv;
-    }
+	}
 }
 
 class SmoArticulation extends SmoNoteModifierBase {
-    static get articulations() {
-        return {
-            accent: 'accent',
-            staccato: 'staccato',
-            marcato: 'marcato',
-            tenuto: 'tenuto',
-            upStroke: 'upStroke',
-            downStroke: 'downStroke',
-            pizzicato: 'pizzicato',
-			fermata:'fermata'
-        };
-    }
-	static get positions() {
-		return {above:'above',below:'below'};
+	static get articulations() {
+		return {
+			accent: 'accent',
+			staccato: 'staccato',
+			marcato: 'marcato',
+			tenuto: 'tenuto',
+			upStroke: 'upStroke',
+			downStroke: 'downStroke',
+			pizzicato: 'pizzicato',
+			fermata: 'fermata'
+		};
 	}
-    static get articulationToVex() {
-        return {
-            accent: 'a>',
-            staccato: 'a.',
-            marcato: 'a^',
-            tenuto: 'a-',
-            upStroke: 'a|',
-            downStroke: 'am',
-            pizzicato: 'ao',
-			fermata:'a@a'
-        };
-    }
+	static get positions() {
+		return {
+			above: 'above',
+			below: 'below'
+		};
+	}
+	static get articulationToVex() {
+		return {
+			accent: 'a>',
+			staccato: 'a.',
+			marcato: 'a^',
+			tenuto: 'a-',
+			upStroke: 'a|',
+			downStroke: 'am',
+			pizzicato: 'ao',
+			fermata: 'a@a'
+		};
+	}
 
-    static get vexToArticulation() {
-        return {
-            "a>": "accent",
-            "a.": "staccato",
-            "a^": "marcato",
-            "a-": "tenuto",
-            "a|": "upStroke",
-            "am": "downStroke",
-            "ao": "pizzicato",
-			'a@a':"fermata"
-        };
-    }
-    static get attrArray() {
-        return ['position', 'articulation'];
-    }
+	static get vexToArticulation() {
+		return {
+			"a>": "accent",
+			"a.": "staccato",
+			"a^": "marcato",
+			"a-": "tenuto",
+			"a|": "upStroke",
+			"am": "downStroke",
+			"ao": "pizzicato",
+			'a@a': "fermata"
+		};
+	}
+	static get attrArray() {
+		return ['position', 'articulation'];
+	}
 
-    static get positionToVex() {
-        return {
-            'above': 3,
-            'below': 4
-        };
-    }
-    static get defaults() {
-        return {
-            position: SmoArticulation.positions.above,
-            articulation: SmoArticulation.articulations.accent
-        };
+	static get positionToVex() {
+		return {
+			'above': 3,
+			'below': 4
+		};
+	}
+	static get defaults() {
+		return {
+			position: SmoArticulation.positions.above,
+			articulation: SmoArticulation.articulations.accent
+		};
 
-    }
-    constructor(parameters) {
-        super('SmoArticulation');
-        Vex.Merge(this, SmoArticulation.defaults);
-        smoMusic.filteredMerge(SmoArticulation.attrArray, parameters, this);
-        this.selector = parameters.selector;
+	}
+	constructor(parameters) {
+		super('SmoArticulation');
+		Vex.Merge(this, SmoArticulation.defaults);
+		smoMusic.filteredMerge(SmoArticulation.attrArray, parameters, this);
+		this.selector = parameters.selector;
 
-        if (!this['attrs']) {
-            this.attrs = {
-                id: VF.Element.newID(),
-                type: 'SmoArticulation'
-            };
-        } else {
-            console.log('inherit attrs');
-        }
-    }
-    get id() {
-        return this.attrs.id;
-    }
-    set id(ignore) {}
-    get type() {
-        return this.attrs.type;
-    }
-    set type(ignore) {}
+		if (!this['attrs']) {
+			this.attrs = {
+				id: VF.Element.newID(),
+				type: 'SmoArticulation'
+			};
+		} else {
+			console.log('inherit attrs');
+		}
+	}
+	get id() {
+		return this.attrs.id;
+	}
+	set id(ignore) {}
+	get type() {
+		return this.attrs.type;
+	}
+	set type(ignore) {}
 }
 
 // ## SmoDynamicText
 // ## Description:
 // standard dynamics text
 class SmoDynamicText extends SmoNoteModifierBase {
-    static get defaults() {
-        return {
-            xOffset: 0,
-            fontSize: 38,
-            yOffsetLine: 11,
-            yOffsetPixels: 0,
-            text: SmoDynamicText.dynamics.MP,
-        };
-    }
+	static get defaults() {
+		return {
+			xOffset: 0,
+			fontSize: 38,
+			yOffsetLine: 11,
+			yOffsetPixels: 0,
+			text: SmoDynamicText.dynamics.MP,
+		};
+	}
 
-    static get dynamics() {
-        // matches VF.modifier
-        return {
-            PP: 'pp',
-            P: 'p',
-            MP: 'mp',
-            MF: 'mf',
-            F: 'f',
-            FF: 'ff',
-            SFZ: 'sfz'
-        };
-    }
+	static get dynamics() {
+		// matches VF.modifier
+		return {
+			PP: 'pp',
+			P: 'p',
+			MP: 'mp',
+			MF: 'mf',
+			F: 'f',
+			FF: 'ff',
+			SFZ: 'sfz'
+		};
+	}
 
-    constructor(parameters) {
-        super('SmoDynamicText');
-        Vex.Merge(this, SmoDynamicText.defaults);
-        smoMusic.filteredMerge(SmoDynamicText.attrArray, parameters, this);
-        this.selector = parameters.selector;
+	constructor(parameters) {
+		super('SmoDynamicText');
+		Vex.Merge(this, SmoDynamicText.defaults);
+		smoMusic.filteredMerge(SmoDynamicText.attrArray, parameters, this);
+		this.selector = parameters.selector;
 
-        if (!this['attrs']) {
-            this.attrs = {
-                id: VF.Element.newID(),
-                type: 'SmoDynamicText'
-            };
-        } else {
-            console.log('inherit attrs');
-        }
-    }
-    get id() {
-        return this.attrs.id;
-    }
-    set id(ignore) {}
-    get type() {
-        return this.attrs.type;
-    }
-    set type(ignore) {}
-    static get attrArray() {
-        return ['xOffset', 'fontSize', 'yOffsetLine', 'yOffsetPixels', 'text'];
-    }
-    backupOriginal() {
-        if (!this['original']) {
-            this.original = {};
-            smoMusic.filteredMerge(
-                SmoDynamicText.attrArray,
-                this, this.original);
-        }
-    }
-    restoreOriginal() {
-        if (this['original']) {
-            smoMusic.filteredMerge(
-                SmoDynamicText.attrArray,
-                this.original, this);
-            this.original = null;
-        }
-    }
+		if (!this['attrs']) {
+			this.attrs = {
+				id: VF.Element.newID(),
+				type: 'SmoDynamicText'
+			};
+		} else {
+			console.log('inherit attrs');
+		}
+	}
+	get id() {
+		return this.attrs.id;
+	}
+	set id(ignore) {}
+	get type() {
+		return this.attrs.type;
+	}
+	set type(ignore) {}
+	static get attrArray() {
+		return ['xOffset', 'fontSize', 'yOffsetLine', 'yOffsetPixels', 'text'];
+	}
+	backupOriginal() {
+		if (!this['original']) {
+			this.original = {};
+			smoMusic.filteredMerge(
+				SmoDynamicText.attrArray,
+				this, this.original);
+		}
+	}
+	restoreOriginal() {
+		if (this['original']) {
+			smoMusic.filteredMerge(
+				SmoDynamicText.attrArray,
+				this.original, this);
+			this.original = null;
+		}
+	}
 }
 
-class SmoTempoText {
-    static get tempoModes() {
-		return {durationMode:'duration',textMode:'text',customMode:'custom'};
+class SmoTempoText extends SmoNoteModifierBase {
+	static get tempoModes() {
+		return {
+			durationMode: 'duration',
+			textMode: 'text',
+			customMode: 'custom'
+		};
+	}
+
+	static get tempoTexts() {
+		return {
+			larghissimo: 'Larghissimo',
+			grave: 'Grave',
+			lento: 'Lento',
+			largo: 'Largo',
+			larghetto: 'Larghetto',
+			adagio: 'Adagio',
+			adagietto: 'Adagietto',
+			andante_moderato: 'Andante moderato',
+			andante: 'Andante',
+			andantino: 'Andantino',
+			moderator: 'Moderato',
+			allegretto: 'Allegretto', 
+			allegro: 'Allegro',
+			vivace: 'Vivace',
+			presto: 'Presto',
+			prestissimo: 'Prestissimo'
+		};
+	}
+
+	static get defaults() {
+		return {
+			tempoMode: SmoTempoText.tempoModes.durationMode,
+			bpm: 120,
+			beatDuration: 4096,
+			tempoText: SmoTempoText.tempoTexts.allegro
+		};
+	}
+	static get attributes() {
+		return ['tempoMode', 'bpm', 'tempoMode', 'beatDuration', 'tempoText'];
+	}
+	constructor(parameters) {
+		super('SmoTempoText');
+		smoMusic.serializedMerge(SmoTempoText.attributes, SmoTempoText.defaults, this);
+		smoMusic.serializedMerge(SmoTempoText.attributes, parameters, this);
 	}
 }
