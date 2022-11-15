c950ece79def65f7585d087e01a9a1c4d0f2a006
AaronDavidNewman <aarondnewman@gmail.com>
2022-11-15 14:57:48 -0600

    fix unit test

--- tests/file-load.ts
+++ tests/file-load.ts
@@ -17,7 +17,7 @@ export function createLoadTests(): void {
     await view.renderPromise();
     QUnit.test('loaded', assert => {
       assert.equal(view.score.staves[0].measures.length, 17);
-      assert.equal($('#boo .vf-lyric').length, 82);
+      assert.equal($('#boo .vf-annotation').length, 82);
     });
     const xml = SmoToXml.convert(view.score);
     const newScore = XmlToSmo.convert(xml);
