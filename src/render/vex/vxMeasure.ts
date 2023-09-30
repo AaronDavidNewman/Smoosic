d26312fe825bc8ad1ab11ae4ca0259549bb16439
AaronDavidNewman <aarondnewman@gmail.com>
2023-09-30 10:25:31 -0500

    fix bug with dots on rests

--- src/render/vex/vxMeasure.ts
+++ src/render/vex/vxMeasure.ts
@@ -92,6 +92,15 @@ export class VxMeasure {
       vexNote.addModifier(acc, tone.pitchIndex);
     });
   }
+  createDots(smoNote: SmoNote, vexNote: Note) {
+    for (var i = 0; i < smoNote.dots; ++i) {
+      for (var j = 0; j < smoNote.pitches.length; ++j) {
+        if (!this.isWholeRest()) {
+          vexNote.addModifier(new VF.Dot(), j);
+        }
+      }
+    }
+  }
   /**
    * Create accidentals based on the active key and previous accidentals in this voice
    * @param smoNote 
@@ -106,6 +115,7 @@ export class VxMeasure {
       return;
     }
     if (smoNote.noteType !== 'n') {
+      this.createDots(smoNote, vexNote);
       return;
     }
     smoNote.accidentalsRendered = [];
@@ -124,13 +134,7 @@ export class VxMeasure {
         smoNote.accidentalsRendered.push('');
       }
     }
-    for (i = 0; i < smoNote.dots; ++i) {
-      for (var j = 0; j < smoNote.pitches.length; ++j) {
-        if (!this.isWholeRest()) {
-          vexNote.addModifier(new VF.Dot(), j);
-        }
-      }
-    }
+    this.createDots(smoNote, vexNote);
     this._createMicrotones(smoNote, vexNote);
     if (smoNote.arpeggio) {
       vexNote.addModifier(new VF.Stroke(smoNote.arpeggio.typeCode));
